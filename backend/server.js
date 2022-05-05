786caa313637ca423755e2c1f8cfa9ef2c375e11
Kiran G <mail@kirang.me>
2022-05-05 23:36:21 +0530

    setup

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,23 @@
 const express = require("express");
-
 const app = express();
+const { chats } = require("./data/data");
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  const singleChat = chats.find((chat) => chat._id === req.params.id);
+  res.send(singleChat);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
