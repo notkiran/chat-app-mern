47bc3a15afa739340f78d445ee4845c5b82b70ed
Kiran G <mail@kirang.me>
2022-05-10 12:56:42 +0530

    jwt

--- backend/server.js
+++ backend/server.js
@@ -4,21 +4,19 @@ const { chats } = require("./data/data");
 const dotenv = require("dotenv");
 const colors = require("colors");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 connectDB();
 
+app.use(express.json());
+
+//Routes
+app.use("/api/user", userRoutes);
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
-
-app.get("/api/chat/:id", (req, res) => {
-  const singleChat = chats.find((chat) => chat._id === req.params.id);
-  res.send(singleChat);
+  res.send("API is Running");
 });
 
 app.listen(PORT, () => {
