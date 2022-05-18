f6b6ac584ab2f55bb148c2a2ac00d386755d485d
Kiran G <mail@kirang.me>
2022-05-18 22:18:55 +0530

    update

--- frontend/src/Components/MyChats.js
+++ frontend/src/Components/MyChats.js
@@ -39,6 +39,7 @@ const MyChats = ({ fetchAgain }) => {
   useEffect(() => {
     setLoggedUser(JSON.parse(localStorage.getItem("userInfo")));
     fetchChats();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [fetchAgain]);
 
   return (
