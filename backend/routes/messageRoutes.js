8271686ab04b6050ff70210c84b6798f4e2860ee
Kiran G <mail@kirang.me>
2022-05-24 23:11:10 +0530

    update

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const { protect } = require("../middleware/authMiddleware");
+const { sendMessage } = require("../controllers/messageControllers.js");
 
 router.route("/").post(protect, sendMessage);
 
