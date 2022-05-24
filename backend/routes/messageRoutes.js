b645afc936c4a92e8ed4bc4a64bae61273f36488
Kiran G <mail@kirang.me>
2022-05-24 23:24:09 +0530

    update

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -5,6 +5,6 @@ const { sendMessage } = require("../controllers/messageControllers.js");
 
 router.route("/").post(protect, sendMessage);
 
-// router.route("/:chatId").get(protect, allMessages);
+router.route("/:chatId").get(protect, allMessages);
 
 module.exports = router;
