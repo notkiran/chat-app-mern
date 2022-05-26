6252fbb86fe81c203a18e480111d9009d3bf4f1c
Kiran G <mail@kirang.me>
2022-05-26 16:43:02 +0530

    socket server

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,17 @@ app.use("/api/message", messageRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`.yellow.bold);
+});
+
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("connected to socket.io");
 });
