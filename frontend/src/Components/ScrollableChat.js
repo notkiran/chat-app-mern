78363c19e3347c7f7d2acb2171011b82d4bd515b
Kiran G <mail@kirang.me>
2022-05-26 16:39:49 +0530

    chat ui

--- frontend/src/Components/ScrollableChat.js
+++ frontend/src/Components/ScrollableChat.js
@@ -1,13 +1,49 @@
+import { Avatar, Tooltip } from "@chakra-ui/react";
 import React from "react";
 import ScrollableFeed from "react-scrollable-feed";
+import {
+  isLastMessage,
+  isSameSender,
+  isSameSenderMargin,
+} from "../Config/ChatLogics";
+import { ChatState } from "../Context/ChatProvider";
 
 const ScrollableChat = ({ messages }) => {
+  const { user } = ChatState();
   return (
     <ScrollableFeed>
       {messages &&
-        messages.map((message, index) => (
-            <div style={{ display: "flex" }} key={message._id}>
-                
+        messages.map((m, i) => (
+          <div style={{ display: "flex" }} key={m._id}>
+            {(isSameSender(messages, m, i, user._id) ||
+              isLastMessage(messages, i, user._id)) && (
+              <Tooltip label={m.sender.name} placement="bottom-start" hasArrow>
+                <Avatar
+                  name={m.sender.name}
+                  src={m.sender.pic}
+                  size="sm"
+                  mr={1}
+                  mt="7px"
+                  cursor="pointer"
+                />
+              </Tooltip>
+            )}
+            <span
+              style={{
+                backgroundColor: `${
+                  m.sender._id === user._id ? "#BEE3F8" : "#B9F5D0"
+                }`,
+                borderRadius: "20px",
+                padding: "5px 15px",
+                maxWidth: "75%",
+                marginLeft: isSameSenderMargin(messages, m, i, user._id),
+                marginTop: isSameSender(messages, m, i, user._id)
+                  ? "3px"
+                  : "10px",
+              }}
+            >
+              {m.content}
+            </span>
           </div>
         ))}
     </ScrollableFeed>
