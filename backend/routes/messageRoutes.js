5775b9e1193787b0c3d8db9cc6d27ba206648fd6
Kiran G <mail@kirang.me>
2022-05-26 13:11:05 +0530

    update

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const { protect } = require("../middleware/authMiddleware");
-const { sendMessage } = require("../controllers/messageControllers.js");
+const {
+  sendMessage,
+  allMessages,
+} = require("../controllers/messageControllers.js");
 
 router.route("/").post(protect, sendMessage);
 
-// router.route("/:chatId").get(protect, allMessages);
+router.route("/:chatId").get(protect, allMessages);
 
 module.exports = router;
