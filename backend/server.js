30389cfdd48047993574b1a67cd06df4367754b3
Kiran G <mail@kirang.me>
2022-05-28 18:24:09 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const path = require("path");
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -15,14 +16,33 @@ connectDB();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is Running");
-});
+// app.get("/", (req, res) => {
+//   res.send("API is Running");
+// });
 
 //Routes
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+
+// -------------- Deployment --------------
+
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  console.log("Production mode");
+  app.use(express.static(path.join(__dirname1, "frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  });
+} else {
+  console.log("Development mode");
+  app.get("/", (req, res) => {
+    res.send("API is Running Successfully");
+  });
+}
+
+// -------------- Error Handling --------------
 
 app.use(notFound);
 app.use(errorHandler);
