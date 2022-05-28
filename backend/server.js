55fe653185d774404bc30fcc67f1f1ac5884e75b
Kiran G <mail@kirang.me>
2022-05-28 21:53:04 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -30,11 +30,11 @@ app.use("/api/message", messageRoutes);
 const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
   console.log("Production mode");
-  app.use(express.static(path.join(__dirname1, "frontend/build")));
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
-  });
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
 } else {
   console.log("Development mode");
   app.get("/", (req, res) => {
